935d5f35b533ea233f54bdfea8c01b5d7e084237
marc2k3 <marc2003@live.co.uk>
2022-04-16 13:42:25 +0100

    Update PanelWindowCUI to use container_uie_window_v3

--- src/Panel/PanelWindowCUI.cpp
+++ src/Panel/PanelWindowCUI.cpp
@@ -3,40 +3,12 @@
 
 namespace
 {
-	class PanelWindowCUI : public PanelWindow, public uie::container_ui_extension, public cui::fonts::common_callback, public cui::colours::common_callback
+	class PanelWindowCUI : public PanelWindow, public uie::container_uie_window_v3, public cui::fonts::common_callback, public cui::colours::common_callback
 	{
 	public:
-		PanelWindowCUI() : m_colours_helper(pfc::guid_null)
+		PanelWindowCUI()
 		{
 			m_is_default_ui = false;
-		}
-
-		HWND create_or_transfer_window(HWND parent, const uie::window_host_ptr& host, const ui_helpers::window_position_t& position) override
-		{
-			if (m_hwnd)
-			{
-				m_hwnd.ShowWindow(SW_HIDE);
-				m_hwnd.SetParent(parent);
-				m_host->relinquish_ownership(m_hwnd);
-				m_host = host;
-
-				m_hwnd.SetWindowPos(nullptr, position.x, position.y, position.cx, position.cy, SWP_NOZORDER);
-			}
-			else
-			{
-				m_host = host;
-				create(parent, this, position);
-			}
-
-			string8 name = pfc::getWindowClassName(parent);
-			m_supports_transparency = name.equals("PSSWindowContainer") || name.equals("ReBarWindow32");
-
-			return m_hwnd;
-		}
-
-		HWND get_wnd() const override
-		{
-			return m_hwnd;
 		}
 
 		IGdiFont* get_font_ui(uint32_t type) override
@@ -51,15 +23,19 @@ namespace
 			switch (msg)
 			{
 			case WM_CREATE:
-				fb2k::std_api_get<cui::colours::manager>()->register_common_callback(this);
-				fb2k::std_api_get<cui::fonts::manager>()->register_common_callback(this);
+				{
+					fb2k::std_api_get<cui::colours::manager>()->register_common_callback(this);
+					fb2k::std_api_get<cui::fonts::manager>()->register_common_callback(this);
+
+					HWND parent = GetParent(hwnd);
+					string8 parent_name = pfc::getWindowClassName(parent);
+					m_supports_transparency = parent_name.equals("PSSWindowContainer") || parent_name.equals("ReBarWindow32");
+				}
 				break;
 			case WM_DESTROY:
 				fb2k::std_api_get<cui::colours::manager>()->deregister_common_callback(this);
 				fb2k::std_api_get<cui::fonts::manager>()->deregister_common_callback(this);
 				break;
-			case WM_SETCURSOR:
-				return 1;
 			case WM_ERASEBKGND:
 				if (is_transparent()) m_hwnd.PostMessageW(uwm::refreshbk);
 				return 1;
@@ -82,20 +58,10 @@ namespace
 			return true;
 		}
 
-		bool is_available(const uie::window_host_ptr&) const override
-		{
-			return true;
-		}
-
 		bool show_config_popup(HWND parent) override
 		{
 			show_configure_popup(parent);
 			return true;
-		}
-
-		class_data& get_class_data() const override
-		{
-			__implement_get_class_data_ex(L"jsp_class_cui", L"", false, 0, WS_CHILD | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, 0, CS_DBLCLKS);
 		}
 
 		const GUID& get_extension_guid() const override
@@ -108,15 +74,14 @@ namespace
 			return to_argb(m_colours_helper.get_colour(static_cast<cui::colours::colour_identifier_t>(type)));
 		}
 
+		uie::container_window_v3_config get_window_config() override
+		{
+			return { L"jsp_class_cui", false, CS_DBLCLKS };
+		}
+
 		uint32_t get_type() const override
 		{
 			return uie::type_toolbar | uie::type_panel;
-		}
-
-		void destroy_window() override
-		{
-			destroy();
-			m_host.release();
 		}
 
 		void get_category(pfc::string_base& out) const override
@@ -132,6 +97,11 @@ namespace
 		void get_name(pfc::string_base& out) const override
 		{
 			out = Component::name;
+		}
+
+		void notify_size_limit_changed() override
+		{
+			get_host()->on_size_limit_change(m_hwnd, uie::size_limit_all);
 		}
 
 		void on_bool_changed(size_t) const override {}
@@ -151,17 +121,8 @@ namespace
 			m_config.set(reader, size, abort);
 		}
 
-		void notify_size_limit_changed() override
-		{
-			if (m_host.is_valid())
-			{
-				m_host->on_size_limit_change(m_hwnd, uie::size_limit_all);
-			}
-		}
-
 	private:
 		cui::colours::helper m_colours_helper;
-		uie::window_host_ptr m_host;
 	};
 
 	static uie::window_factory<PanelWindowCUI> g_panel_window_cui;
