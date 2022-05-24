bfdf1f9a9c867651b9044824db1ed8f4621e4e97
marc2k3 <marc2003@live.co.uk>
2022-05-24 15:59:41 +0100

    v2.8.7

--- src/foo_jscript_panel.h
+++ src/foo_jscript_panel.h
@@ -3,10 +3,10 @@
 namespace Component
 {
 	static const std::string name = "JScript Panel";
-	static const std::string version = "2.8.6.2";
+	static const std::string version = "2.8.7";
 	static const std::string name_version = name + " v" + version;
 	static const std::string home_page = "https://github.com/marc2k3/foo_jscript_panel";
-	static constexpr uint32_t version_number = 2862;
+	static constexpr uint32_t version_number = 2870;
 
 	std::string get_resource_text(int id);
 	std::wstring get_path();
